d56653ec3b7cd8c455000287926097196642ad4c
DJ-lulu <luciemnv@gmail.com>
2023-11-27 16:33:17 +0100

    MAJ PA LAT

--- mini_manche/test/simu_pa_lat.py
+++ mini_manche/test/simu_pa_lat.py
@@ -1,9 +1,14 @@
 import ivy.std_api as ivy
-
+import time
+from ivy.std_api import *
 
 ### Ivy Bus
-app_name = " MyIvyApplication "
-ivy_bus = " 127.255.255.255:2010 "
+def on_cx_proc (agent , connected ) :
+    pass
+def on_die_proc (agent , _id ):
+    pass
+app_name = "MyIvyApplication"
+ivy_bus = "127.255.255.255:2010"
 IvyInit ( app_name , # application name for Ivy
 "[%s ready ]" % app_name , # ready message
 0, # main loop is local
@@ -12,18 +17,18 @@ on_die_proc ) # called when the agent dies
 IvyStart ( ivy_bus )
 
 ###Test des constantes
-PConstanteVirage = 0.5
-PconstanteLigneDroite = 0
-
+PConstanteVirage=0.5
+PconstanteLigneDroite=0
 
 ### Envoie message Ivy
-import time
-from ivy . std_api import *
+
 def null_cb (*a):
     pass
-IvyInit (" IvyApp ", " Ready ", 0 , null_cb , null_cb )
-IvyStart (" 127.255.255.255:2010 ")
 # attente pour qu ’ivy s’ initialise correctement
 time . sleep (1.0)
-IvySendMsg (" PConstanteVirage = 0.5 PconstanteLigneDroite = 0  ")
+IvySendMsg ("PConstanteVirage=0.5 PconstanteLigneDroite=0")
 IvyStop ()
+
+
+
+
