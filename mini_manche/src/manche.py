62250c41c1e9d8d54d89293e1e26cefc9ad9c88e
Vitry Hugo <hugo.louis.vitry@gmail.com>
2023-12-11 17:30:44 +0100

    latence réduite

--- mini_manche/src/manche.py
+++ mini_manche/src/manche.py
@@ -3,6 +3,8 @@ import time
 import ivy.std_api as ivy
 import pygame
 from math import pi, radians
+import threading as tr
+import sys
 clock_interval = 30
 
 
@@ -10,63 +12,101 @@ clock_interval = 30
 Pmax= radians(5) # 5 degrés/sec
 nzmax=1
 
+
 # Variables globales 
+
 Manual=False
 p=0
 nz=1
+gachette=0
+
 pygame.init()
 
-def from_stick(agent, *larg):
+def update_stick():
     """Récupération des données brutes du joystick \n Modifie nz,p AP off
-    """    
-    clock = pygame.time.Clock()
-    init = False
-    axe_p  = 0
-    axe_nz = 1
-    axe_nx = 2
+    """
     
-    joystick = pygame.joystick.Joystick(0)
+    joysticks = {}
+    done=False
+    while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True  # Flag that we are done so we exit this loop.
 
-    # Positions angulaires
-    brut_p = joystick.get_axis(axe_p)
-    brut_nz = joystick.get_axis(axe_nz)
-    brut_nx = joystick.get_axis(axe_nx) # nx ou flap ????
+                if event.type == pygame.JOYBUTTONDOWN:
+                    if event.button == 0:
+                        joystick = joysticks[event.instance_id]
+                        if joystick.rumble(0, 0.7, 500):
+                            print(f"Rumble effect played on joystick {event.instance_id}")
 
-    # Boutons
-    gachette = joystick.get_button(0)
-    bouton_gauche=joystick.get_button(1) # Landing gear ??
-    bouton_droit=joystick.get_button(2) # change nx , flap ??
+                # Handle hotplugging
+                if event.type == pygame.JOYDEVICEADDED:
+                    # This event will be generated when the program starts for every
+                    # joystick, filling up the list without needing to create them manually.
+                    joy = pygame.joystick.Joystick(event.device_index)
+                    joysticks[joy.get_instance_id()] = joy
+                    print(f"Joystick {joy.get_instance_id()} connected")
 
-    #Debug
-    # print(f"bp : {brut_p}\t bnz : {brut_nz}\t bnx : {brut_nx}\t bGachette : {gachette}\t bBGauche : {bouton_gauche}\t bBDroit : {bouton_droit}\n")
+                if event.type == pygame.JOYDEVICEREMOVED:
+                    del joysticks[event.instance_id]
+                    print(f"Joystick {event.instance_id} disconnected")
+        axe_p  = 0
+        axe_nz = 1
+        axe_nx = 2
 
-    #Traitement de l'information
+        global nz
+        global p
+        global Manual
+        global gachette
+
+        for joystick in joysticks.values():
+
+            # Positions angulaires
+            brut_p = joystick.get_axis(axe_p)
+            brut_nz = joystick.get_axis(axe_nz)
+            brut_nx = joystick.get_axis(axe_nx) # nx ou flap ????
+
+            # Boutons
+            gachette = joystick.get_button(0)
+            bouton_gauche=joystick.get_button(1) # Landing gear ??
+            bouton_droit=joystick.get_button(2) # change nx , flap ??
+
+            #Debug
+            #print(f"bp : {brut_p}\t bnz : {brut_nz}\t bnx : {brut_nx}\t bGachette : {gachette}\n")# bBGauche : {bouton_gauche}\t bBDroit : {bouton_droit}\n")
+
+            #Traitement de l'information
+            nz = brut_nz*nzmax
+            p = brut_p * Pmax
+
+            #Dead zone
+            if abs(brut_p) <= 0.13 :
+                p = 0
+            if abs(brut_nz) <= 0.1 :
+                nz = 0
+
+
+def ivy_share(agent, *larg):
+    
     global nz
     global p
     global Manual
-
-    nz = brut_nz*nzmax
-    p = brut_p * Pmax
-
-    #Dead zone
-    if abs(brut_p) <= 0.1 :
-        p = 0
-    if abs(brut_nz) <= 0.1 :
-        nz = 0
-
+    global gachette
+    
     #Désactivation autopilote
     if  not Manual and gachette==1:
         Manual = True
+        print(f"Manual : {Manual}")
         ivy.IvySendMsg("MancheAP push")
 
     #Envoi des commandes manuelles
     if Manual :
         ivy.IvySendMsg(f"MancheCmdAxes nz={nz+1} p={p}")
 
+
 def on_AP_pushed(agent, *larg):
     global Manual
     Manual = False
-
+    print(f"Manual : {Manual}")
 
 #
 def on_cx_proc(agent, connected):
@@ -76,9 +116,12 @@ def on_die_proc(agent,_id):
     pass
 
 app_name = "Manche"
-default_ivy_bus = "127.255.255.255:2010"
+ivy_bus = "127.255.255.255:2010"
 
-def launch_manche(ivy_bus=default_ivy_bus):
+if len(sys.argv)==2:
+    ivy_bus = sys.argv[1]
+
+def launch_manche():
     """
     Default bus is 127.255.255.255:2010\n
     Enter the bus as a string "adrress"
@@ -87,8 +130,10 @@ def launch_manche(ivy_bus=default_ivy_bus):
     ivy.IvyInit( app_name , "[%s ready ]" % app_name , 0, on_cx_proc ,on_die_proc ) 
     ivy.IvyStart ( ivy_bus )
     time.sleep(1.0)
-    ivy.IvyBindMsg(from_stick,"^Time t=(\S+)")
+    stick=tr.Thread(target=update_stick)
+    stick.start()
     ivy.IvyBindMsg(on_AP_pushed,"^FCUAP1 on")
+    ivy.IvyBindMsg(ivy_share,"^Time t=(\S+)")
     print("Ready")
 
 if __name__ == "__main__":
