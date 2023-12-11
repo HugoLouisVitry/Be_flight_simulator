279f72d19490bbc8a6d0b719ee510f57d15207e0
DJ-lulu <luciemnv@gmail.com>
2023-12-11 14:16:18 +0100

    simu_perfo

--- mini_manche/test/simu_perfo.py
+++ mini_manche/test/simu_perfo.py
@@ -21,11 +21,20 @@ def on_time(agent, *larg):
         nz_neg=-1
         nx_pos=1
         nz_pos=1
-        
     IvySendMsg (f"nx_neg={nx_neg} nz_neg={nz_neg} nx_pos={nx_pos} nz_pos={nz_pos}")
 def on_message(agent, *larg):
     IvySendMsg ("APNxControl nx=1")
     IvySendMsg ("APNzControl nz=1")
+    IvySendMsg("MagneticDeclination=(13,31)")
+    IvySendMsg("WindComponent VWind=(\S+) dirWind=(\S+)")
+    IvySendMsg("LimitsN nx_neg=(\S+) nx_pos=(\S+) nz_neg=(\S+) nz_pos=(\S+)")
+    IvySendMsg("LimitsNAP nx_neg_AP=(\S+) nx_pos_AP=(\S+) nz_neg_AP=(\S+) nz_pos_AP=(\S+)")
+    IvySendMsg("SpeedLimits vmin=(\S+) vmax=(\S+)")
+    IvySendMsg("ManagedSpeed vi=(\S+)")
+    IvySendMsg("RollLimits phimax=(\S+) pmax=(\S+)")
+    IvySendMsg("RollLimitsAP phimax_AP=(\S+) pmax_AP=(\S+)")
+    IvySendMsg("Axis x=(\S+) y=(\S+) chi=(\S+)")
+    IvySendMsg("ManagedAlt alt=(\S+) Q=(\S+)")
 
 ### Ivy Bus
 app_name = "Minimancheperfo "
@@ -38,8 +47,6 @@ on_die_proc ) # called when the agent dies
 IvyStart ( ivy_bus )
 IvyBindMsg(on_time, '^Time t=(.*)')
 IvyBindMsg(on_message, "^StateVector x=(\S+) y=(\S+) z=(\S+) Vp=(\S+) fpa=(\S+) psi=(\S+) phi=(\S+)")
-
-
 
 # attente pour qu ’ivy s’ initialise correctement
 time . sleep (1.0)
