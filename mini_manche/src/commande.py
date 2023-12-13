1577c85e764e91fbfdbbf58090d14db77545a9fb
Quentin Lutter <quentinlutter1@gmail.com>
2023-12-13 17:40:29 +0100

    modification launch.py

--- mini_manche/src/commande.py
+++ mini_manche/src/commande.py
@@ -15,17 +15,23 @@ def on_die_proc(agent, _id):
     pass
 
 # Constante
-Dt = 1 # Constante de temps pour lisser la limitation dû à phi
+Dt = 2 # Constante de temps pour lisser la limitation dû à phi_limite_FGS
+back_angle = math.radians(33)
+marge_angle = math.radians(5)
+tau_back = 1 # Tau pour le retour à 33° de roulis en manuel
 
-# Variable globale
-autoPiloteStatus = "off"
+# Variable globale reçue
 nz_manche = 1. # Commande brute
 p_manche = 0. # Commande brute
-nz_max = 1. # Limite brute
-nz_min = 1. # Limite brute
+nz_max = 2. # Limite brute
+nz_min = 0. # Limite brute
 phi_limite_FGS = math.pi/4 # en radians
 p_limite_FGS = math.pi/16 # en radians/s
 phi = 0. # en radians
+
+# Variable globale interne
+autoPiloteStatus = "off"
+back_to_rest = False # Booléen pour le retour à +/- 33° de roulis au lâcher du mini manche
 
 # Fonction message
 def on_MancheAP(agent, *larg): # Désactive l'autopilote en cas d'appuie du bouton au manche
@@ -53,8 +59,8 @@ def on_LimitsN(agent,*larg): # Ignore la limitation en nx car non commandé par 
 
 def on_RollLimits(agent,*larg):
     global phi_limite_FGS, p_limite_FGS
-    phi_limite_FGS = math.radians(larg[0]) # Convertion deg -> rad nécessaire
-    p_limite_FGS = math.radians(larg[1])
+    phi_limite_FGS = math.radians(float(larg[0])) # Convertion deg -> rad nécessaire
+    p_limite_FGS = math.radians(float(larg[1]))
 
 def on_StateVector(agent,*larg): # Stocke la valeur de phi
     global phi  
@@ -73,6 +79,7 @@ def on_APLongNzControl(agent, *larg):
     ivy.IvySendMsg(f"APNzControl nz={nz}")
 
 def on_APLatpControl(agent,*larg):
+    global back_to_rest
     p_AP = float(larg[0])
     if autoPiloteStatus == "on":
         p = p_AP
@@ -88,10 +95,21 @@ def on_APLatpControl(agent,*larg):
         elif phi > phi_limite_FGS:
             p = phi_limite_FGS - phi
 
+        # Retour à p = +/- 33° si on relâche le manche après être arrivé en butée
+        if p_manche == 0 and back_to_rest :
+            if phi < 0: # On suppose que l'on ne dépasse pas 0 lorsque l'on commande phi = +/- 33°
+                p = min((-phi - back_angle)/tau_back,p_limit_pos)
+            else:
+                p = max((back_angle - phi)/tau_back,p_limit_neg)
+        
+        if abs(abs(phi) - phi_limite_FGS) <= marge_angle : # On arrive en butée de phi -> armement du retour à +/- 33°
+            back_to_rest = True
+        elif p_manche != 0 : # On n'est plus/pas en butée et il y a une commande manuel -> désarmement du retour à +/- 33°
+            back_to_rest = False
+
     ivy.IvySendMsg(f"APLatControl rollRate={p}")
             
 
-#TODO Ajouter la limitation de roulis (avec retour à +/- 33°) 
 
 def commande(ivy_bus=Ivy_bus):
     """
